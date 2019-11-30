6fe09e313d23fe893b6eabb65c8573b75d4fda62
Ubuntu <ubuntu@industry.gate>
2019-11-30 23:31:01 +0900

    website

---
+++ ig.php
@@ -0,0 +1,49 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1"/>
+<title>Industry Gate</title>
+<style>
+html {
+	height: 100%;
+}
+
+body {
+	height: 90%;
+	background-color: #E0FFFF;
+}
+
+.title-text {
+	font-family: "Gill Sans", sans-serif;
+	text-align: center;
+	margin: auto; !important;
+	margin-bottom: 10px;
+	padding: 5px;
+	width: 100%;
+}
+
+#title-big {
+	font-size: 50px;
+	color: #2F4F4F;
+	font-weight: 700;
+}
+
+#title-small {
+	color: #556B2F;
+}
+
+.container {
+	margin: auto; !important;
+	margin-top: 30px;
+	height: 40px;
+	width: 300px;
+	text-align: center;
+}
+
+.content {
+	height: 100%;
+	margin-top: 20px;
+}
