9944cb82291de8513794d11dd4cd1a0b2ec36c84
a52young <root@industry.gate>
2019-12-07 16:49:37 +0900

    aply bootstrap

--- ig_cp.php
+++ ig_cp.php
@@ -11,6 +11,12 @@ if($_GET[select_type] != "") {
 <head>
 <meta charset="UTF-8"/>
 <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1"/>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384- Vkoo8x4CGsO3+ Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384- wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+<script src="https://code.jquery.com/jquery- 3.4.1.slim.min.js"integrity="sha384- J6qa4849blE2+ poT4WnyKhv5vZF5SrPoOiEjwBvKU7imGFAV0wwj1yYfoRSJoZ+ n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384- Q6E9RHvblyZFJoft+ 2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+
 <title>Industry Gate</title>
 
 <style>
@@ -221,7 +227,7 @@ $(document).ready(function() {
 			<div class="left-content">
 			</div>
 			<div class="right-content">
-				<table class="content-table">
+				<table class="content-table table table-striped">
 					<thead>
 					<tr>
 						<th class="th-ele">number</th>
